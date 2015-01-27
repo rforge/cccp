8a555485091a629876b760b29673d790546019eb
bpfaff <bpfaff@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-27 21:53:43 +0000

    Removing current version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cccp@143 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gets.R
+++
@@ -1,8 +0,0 @@
-##
-## Value of variable 's'
-setMethod("gets", signature = "PDV", function(object){
-    lapply(object@s, function(s) umat(s)@u)
-})
-setMethod("gets", signature = "CPS", function(object){
-    lapply(object@s, function(s) umat(s)@u)
-})
